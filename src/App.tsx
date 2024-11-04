1114555260a09ae4ccebd951cb3048fa7069489f
Klomachenko <leekyumin0901@naver.com>
2024-11-04 21:45:20 +0900

    chore: emotion-styled 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DefaultButton from './components/DefaultButton';
 
 function App() {
-  return <div className='App'>소공론 프로젝트</div>;
+  return (
+    <div className='App'>
+      <DefaultButton />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DefaultButton.tsx
@@ -0,0 +1,5 @@
+const DefaultButton = () => {
+  return <button>버튼입니다</button>;
+};
+
+export default DefaultButton;
