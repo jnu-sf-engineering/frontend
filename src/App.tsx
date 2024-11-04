2ed5cb390d1d22beb7509461dbdfba58b4c3c2d1
Klomachenko <leekyumin0901@naver.com>
2024-11-04 21:58:44 +0900

    feat: DefaultButton 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import DefaultButton from './components/DefaultButton';
 function App() {
   return (
     <div className='App'>
-      <DefaultButton />
+      <DefaultButton text='버튼 내용 입력' width='10rem' height='3.125rem' />
     </div>
   );
 }
--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -1,5 +1,36 @@
-const DefaultButton = () => {
-  return <button>버튼입니다</button>;
+import styled from '@emotion/styled';
+import React from 'react';
+
+interface ButtonProps {
+  width?: string;
+  height?: string;
+  text?: string;
+}
+
+const MainButton = styled.button<ButtonProps>`
+  width: ${({ width }) => width || '10.3125rem'};
+  height: ${({ height }) => height || '3.125rem'};
+  font-size: 1.3125rem;
+  background-color: #88afe3;
+  cursor: pointer;
+  color: white;
+  transition: background-color 0.3s, color 0.3s;
+  border: none;
+  border-radius: 0.5rem;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 0.1rem solid #88afe3;
+  }
+`;
+
+const DefaultButton: React.FC<ButtonProps> = ({ width, height, text }) => {
+  return (
+    <MainButton width={width} height={height}>
+      {text}
+    </MainButton>
+  );
 };
 
 export default DefaultButton;
