7a777646c21428acec408a835853438b98dbe3cb
Klomachenko <leekyumin0901@naver.com>
2024-11-05 16:37:31 +0900

    feat: component 작업을 위한 route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import DefaultButton from './components/DefaultButton';
-import TaskCard from './components/TaskCard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Yunn from './pages/Yunn';
+import Klomachenko from './pages/Klomachenko';
 
 function App() {
   return (
-    <div className='App'>
-      {/* <DefaultButton text='버튼 내용 입력' width='10rem' height='3.125rem' /> */}
-      <TaskCard
-        taskContent='API를 상의 후에 설계합니다. API를 상의 후에 설계합니다.API를 상의 후에 설계합니다.API를 상의 후에 설계합니다.API를 상의 후에 설계합니다.API를 상의 후에 설계합니다.API를 상의 후에 설계합니다.API를 상의 후에 설계합니다.'
-        authorName='박경준'
-      />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/yunn' element={<Yunn />} />
+          <Route path='/min' element={<Klomachenko />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
