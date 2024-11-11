3d42b08cac13e72d0d34b666892fc08ada452739
JeongSeoyun <xeonj@naver.com>
2024-11-11 21:39:54 +0900

    feat: ProjectList 컴포넌트 구현

---
+++ src/mocks/ProjectListMock.ts
@@ -0,0 +1,20 @@
+const ProjectListMock = {
+  success: true,
+  response: [
+    {
+      projectId: 1,
+      projectName: '프로젝트1',
+      sprintCount: null,
+      manager: '정서윤'
+    },
+    {
+      projectId: 2,
+      projectName: '프로젝트2',
+      sprintCount: 2,
+      manager: '정서윤'
+    }
+  ],
+  error: null
+}
+  
+export default ProjectListMock
