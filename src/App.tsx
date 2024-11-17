98f739c24af4cf5a98b25c63afa7db18756a9451
JeongSeoyun <xeonj@naver.com>
2024-11-17 17:48:34 +0900

    feat: 모든 페이지에 NavBar 상단 고정

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import Klomachenko from './pages/Klomachenko';
 import Project from './pages/Project';
 import Retro from './pages/Retro';
 import LastSprint from './pages/LastSprint';
+import Login from './pages/Login';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path='/yunn' element={<Yunn />} />
           <Route path='/min' element={<Klomachenko />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/project' element={<Project />} />
           <Route path='/retro' element={<Retro />} />
           <Route path='/lastsprint' element={<LastSprint />} />
